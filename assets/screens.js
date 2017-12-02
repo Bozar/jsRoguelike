1bd3a8cc63ee08f5367f075078d74e29eb3423aa
bozar <tommyfrnaklin42@outlook.com>
2017-12-02 18:58:42 +0800

    Change Game.Screen into a constructor

--- assets/screens.js
+++ assets/screens.js
@@ -1,56 +1,58 @@
 'use strict'
 /* global Game */
 
-Game.Screen = {}
+Game.Screen = function () { }
+Game.Screen.prototype.enter = function () { }
+Game.Screen.prototype.exit = function () { }
+Game.Screen.prototype.render = function () { }
+Game.Screen.prototype.handleInput = function () { }
 
-Game.Screen.start = {}
-Game.Screen.start.enter = function () { console.log('enter: start') }
-Game.Screen.start.exit = function () { console.log('exit: start') }
-Game.Screen.start.render = function (display) {
+Game.Screen.startScreen = new Game.Screen()
+Game.Screen.startScreen.enter = function () { console.log('enter: start') }
+Game.Screen.startScreen.exit = function () { console.log('exit: start') }
+Game.Screen.startScreen.render = function (display) {
   display.drawText(1, 1, '%c{yellow}Roguelike Javascript')
   display.drawText(1, 2, 'Press [Enter]')
 }
-Game.Screen.start.handleInput = function (eventType, inputKey) {
+Game.Screen.startScreen.handleInput = function (eventType, inputKey) {
   if (eventType === 'keydown') {
     if (inputKey.code === 'Enter') {
-      Game.switchScreen(Game.Screen.play)
+      Game.switchScreen(Game.Screen.playScreen)
     } else {
       console.log('start: incorrect key: ' + inputKey.code)
     }
   }
 }
 
-Game.Screen.play = {}
-Game.Screen.play.enter = function () { console.log('enter: play') }
-Game.Screen.play.exit = function () { console.log('exit: play') }
-Game.Screen.play.render = function (display) {
+Game.Screen.playScreen = new Game.Screen()
+Game.Screen.playScreen.enter = function () { console.log('enter: play') }
+Game.Screen.playScreen.exit = function () { console.log('exit: play') }
+Game.Screen.playScreen.render = function (display) {
   display.drawText(1, 1, 'Press [Enter] to win')
   display.drawText(1, 2, 'Press [Esc] to lose')
 }
-Game.Screen.play.handleInput = function (eventType, inputKey) {
+Game.Screen.playScreen.handleInput = function (eventType, inputKey) {
   if (eventType === 'keydown') {
     if (inputKey.code === 'Enter') {
-      Game.switchScreen(Game.Screen.win)
+      Game.switchScreen(Game.Screen.winScreen)
     } else if (inputKey.code === 'Escape') {
-      Game.switchScreen(Game.Screen.lose)
+      Game.switchScreen(Game.Screen.loseScreen)
     } else {
       console.log('play: incorrect key: ' + inputKey.code)
     }
   }
 }
 
-Game.Screen.win = {}
-Game.Screen.win.enter = function () { console.log('enter: win') }
-Game.Screen.win.exit = function () { console.log('exit: win') }
-Game.Screen.win.render = function (display) {
+Game.Screen.winScreen = new Game.Screen()
+Game.Screen.winScreen.enter = function () { console.log('enter: win') }
+Game.Screen.winScreen.exit = function () { console.log('exit: win') }
+Game.Screen.winScreen.render = function (display) {
   display.drawText(1, 1, 'you win')
 }
-Game.Screen.win.handleInput = function () {}
 
-Game.Screen.lose = {}
-Game.Screen.lose.enter = function () { console.log('enter: lose') }
-Game.Screen.lose.exit = function () { console.log('exit: lose') }
-Game.Screen.lose.render = function (display) {
+Game.Screen.loseScreen = new Game.Screen()
+Game.Screen.loseScreen.enter = function () { console.log('enter: lose') }
+Game.Screen.loseScreen.exit = function () { console.log('exit: lose') }
+Game.Screen.loseScreen.render = function (display) {
   display.drawText(1, 1, 'you lose')
 }
-Game.Screen.lose.handleInput = function () {}
