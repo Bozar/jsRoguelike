3f37b6ca25f890425869671ed713ea725a959df4
bozar <tommyfranklin42@outlook.com>
2017-12-11 21:43:46 +0800

    Add mixin: Game.Mixins.Movealbe

---
+++ assets/entities.js
@@ -0,0 +1,21 @@
+'use strict'
+/* global Game */
+
+Game.Mixins = {}
+
+Game.Mixins.Movealbe = {
+  name: 'Movealbe',
+  tryMove: function (x, y, map) {
+    let tile = map.getTile(x, y)
+    if (tile.isWalkable()) {
+      this._x = x
+      this._y = y
+      return true
+    } else if (tile.isDiggable()) {
+      map.dig(x, y)
+      return true
+    } else {
+      return false
+    }
+  }
+}
