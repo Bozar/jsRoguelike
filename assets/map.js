6f646a7c88acbe7bcc31817ba0c8443e39b97c9e
Bozar <tommyfranklin42@yahoo.com>
2017-12-11 17:43:15 +0800

    Add methods to Game.Map: dig, getRandomFloorPosition

--- assets/map.js
+++ assets/map.js
@@ -1,11 +1,26 @@
 'use strict'
-/* global Game */
+/* global Game x y */
 
 Game.Map = function (tiles) {
   // tiles: 2D array, [[column 1], [column 2], ...]
   this._tiles = tiles
   this._width = tiles.length
   this._height = tiles[0].length
+}
+
+Game.Map.prototype.dig = function () {
+  if (this.getTile.isDiggable) {
+    this._tiles[x][y] = Game.tiles.floorTile
+  }
+}
+
+Game.Map.prototype.getRandomFloorPosition = function () {
+  let x, y
+  do {
+    x = Math.floor(Math.random(x * this._width))
+    y = Math.floor(Math.random(x * this._height))
+  } while (this._tiles[x][y] !== Game.Tile.floorTile)
+  return {x: x, y: y}
 }
 
 Game.Map.prototype.getWidth = function () { return this._width }
