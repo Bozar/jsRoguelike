fbb236d410c8c03d14e066901da7907ebd4b9616
Bozar <tommyfranklin42@outlook.com>
2017-12-14 10:26:16 +0800

    Create actors: player & fungus

--- assets/entities.js
+++ assets/entities.js
@@ -10,7 +10,10 @@ Game.Mixins.Movealbe = {
   name: 'Movealbe',
   tryMove: function (x, y, map) {
     let tile = map.getTile(x, y)
-    if (tile.isWalkable()) {
+    let target = map.getEntityAt(x, y)
+    if (target) {
+      return false
+    } else if (tile.isWalkable()) {
       this._x = x
       this._y = y
       return true
@@ -23,7 +26,28 @@ Game.Mixins.Movealbe = {
   }
 }
 
+Game.Mixins.PlayerActor = {
+  name: 'PlayerActor',
+  groupName: 'Actor',
+  act: function () {
+    Game.redrawScreen()
+    this.getMap().getEngine().lock()
+  }
+}
+
+Game.Mixins.FungusActor = {
+  name: 'FungusActor',
+  groupName: 'Actor',
+  act: function () { }
+}
+
 Game.PlayerTemplate = {
   character: '@',
-  mixins: [Game.Mixins.Movealbe]
+  mixins: [Game.Mixins.Movealbe, Game.Mixins.PlayerActor]
+}
+
+Game.FungusTemplate = {
+  character: 'F',
+  foreground: 'green',
+  mixins: [Game.Mixins.FungusActor]
 }
